1a5a4e6b05bf59c08034e859afb874abb599296c
Dmitry <Atlantiskun@gmail.com>
2022-04-25 17:26:36 +0700

    remove unused code

--- SwiftUI_Examples/ContentView.swift
+++ SwiftUI_Examples/ContentView.swift
@@ -18,38 +18,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-
-//@State var show = false
-
-//Button {
-//    withAnimation {
-//        self.show.toggle()
-//    }
-//} label: {
-//    VStack {
-//        Text("Learn SwiftUI")
-//            .foregroundColor(.white)
-//            .fontWeight(.bold)
-//            .font(.largeTitle)
-//            .padding(.top, show ? 100 : 20)
-//
-//        Text("A course on UI and animations")
-//            .foregroundColor(Color(hue: 0.567, saturation: 0.158, brightness: 0.943))
-//            .lineLimit(-1)
-//
-//        Spacer()
-//
-//        Text("Card Animation")
-//            .foregroundColor(Color(hue: 0.498, saturation: 0.609, brightness: 1))
-//            .fontWeight(.bold)
-//            .font(.title)
-//            .padding(.bottom, show ? 100 : 20)
-//    }
-//    .padding()
-//    .frame(width: show ? 350 : 300, height: show ? 600 : 300)
-//    .background(Color.blue)
-//}
-//.cornerRadius(30)
-//.animation(.spring())
-//.shadow(radius: 30)
